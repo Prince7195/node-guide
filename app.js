be12ed2fe5f7844aef9327d6901237000ed4fe6e
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-01-15 17:34:05 +0530

    added template engine and changed html to ejs files

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 // Local imports
-const adminRoute = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
 // Global Variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use("/admin", adminRoute);
+app.use("/admin", adminData.routes);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404", { pageTitle: "Page Not Found", path: "" });
 });
 
 // Server listening
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,17 @@
 const path = require("path");
 const express = require("express");
 
-const roorDir = require("../util/path");
-
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(roorDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/"
+  });
 });
 
 module.exports = router;
