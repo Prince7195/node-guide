774d3528201d3f9ea056289d3626e6ce4201604a
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-01-19 14:48:51 +0530

    dynamic routing and updated all pages with content

--- routes/admin.js
+++ routes/admin.js
@@ -4,15 +4,21 @@ const express = require("express");
 const router = express.Router();
 
 const adminController = require("../controllers/admin");
-const { getAddProducts, postAddProducts, getProducts } = adminController;
+const { getAddProducts, postAddProducts, getProducts, getEditProduct, postEditProduct, postDeleteProduct } = adminController;
+
+// /admin/products - GET
+router.get("/products", getProducts);
 
 // /admin/add-product - GET
 router.get("/add-product", getAddProducts);
 
-// /admin/add-product - POST
+// /admin/add-product - POST,
 router.post("/add-product", postAddProducts);
 
-// /admin/products - GET
-router.get("/products", getProducts);
+router.get("/edit-product/:productId", getEditProduct);
+
+router.post("/edit-product", postEditProduct);
+
+router.post("/delete-product", postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,13 +4,19 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
-const { getProducts, getIndex, getCart, getCheckout, getOrders } = shopController;
+const { getProducts, getIndex, getCart, getCheckout, getOrders, getProduct, postCart, postCartDeleteProduct } = shopController;
 
 router.get("/", getIndex);
 
 router.get("/products", getProducts);
 
+router.get("/products/:productId", getProduct);
+
 router.get("/cart", getCart);
+
+router.post("/cart", postCart);
+
+router.post("/cart-delete-item", postCartDeleteProduct);
 
 router.get("/orders", getOrders);
 
