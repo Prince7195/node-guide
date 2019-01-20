f30e8847684247a9cd6efb9e954ce217fd22b911
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-01-20 20:49:10 +0530

    changed db from sql to mongo and modified till products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require("mongodb");
 const Product = require("../models/product");
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProducts = (req, res, next) => {
     res.render("admin/edit-product", {
@@ -13,13 +16,8 @@ exports.postAddProducts = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    req.user
-        .createProduct({
-            title,
-            imageUrl,
-            price,
-            description
-        })
+    const product = new Product(title, price, description, imageUrl);
+    product.save()
         .then(() => {
             res.redirect("/admin/products");
         }).catch(err => {
@@ -56,17 +54,8 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
-    Product.findById(prodId)
-        .then((product) => {
-            if (!product) {
-                return res.redirect("/");
-            }
-            product.title = updatedTitle;
-            product.price = updatedPrice;
-            product.imageUrl = updatedImageUrl;
-            product.description = updatedDescription;
-            return product.save();
-        })
+    const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, new ObjectId(prodId));
+    product.save()
         .then(() => {
             res.redirect("/admin/products");
         })
@@ -81,19 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    // It also works
-    // Product.destroy({ where: { id: prodId } })
-    //     .then((deletedRowCount) => {
-    //         console.log(deletedRowCount);
-    //         res.redirect("/admin/products");
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     });
-    Product.findById(prodId)
-        .then(product => {
-            return product.destroy();
-        })
+    Product.deleteById(prodId)
         .then(() => {
             res.redirect("/admin/products");
         })
@@ -103,8 +80,7 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    req.user
-        .getProducts()
+    Product.fetchAll()
         .then(products => {
             res.render("admin/products", {
                 prods: products,
--- util/database.js
+++ util/database.js
@@ -1,5 +1,29 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-guide", "root", "password", { dialect: "mysql", host: "localhost" });
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+
+    MongoClient.connect(
+        "mongodb+srv://user:user@cluster0-uyecl.mongodb.net/shop?retryWrites=true",
+        { useNewUrlParser: true })
+        .then(client => {
+            console.log("Mongo Atlas Connected");
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw "No DB found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
