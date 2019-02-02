f4d40d7bee028a3f132799aa260f0cce418ef57b
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-02-02 12:41:18 +0530

    updated authorization and Access restrictions

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getLogin, postLogin, postLogout, getSignup, postSignup } = require("../controllers/auth");
+const { getLogin, postLogin, postLogout, getSignup, postSignup, getReset, postReset, getNewPassword, postNewPassword } = require("../controllers/auth");
 
 const router = express.Router();
 
@@ -7,10 +7,18 @@ router.get("/login", getLogin);
 
 router.get("/signup", getSignup);
 
+router.get("/reset", getReset);
+
+router.get("/reset/:token", getNewPassword);
+
 router.post("/login", postLogin);
 
 router.post("/signup", postSignup);
 
 router.post("/logout", postLogout);
+
+router.post("/reset", postReset);
+
+router.post("/new-password", postNewPassword);
 
 module.exports = router;
